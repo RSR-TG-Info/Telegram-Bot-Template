d2a76aa76574da90960709a8525a40de20355897
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2023-06-06 18:35:35 +0530

    Update rsrconfig.py

--- rsrconfig.py
+++ rsrconfig.py
@@ -1,7 +1,7 @@
 class Config(object):
 
     #Get a bot token from botfather
-    TG_BOT_TOKEN = "Bot token" #Replace Bot token
+    BOT_TOKEN = "Bot token" #Replace Bot token
 
     #Get from my.telegram.org
     API_ID = API ID #Replace API ID
