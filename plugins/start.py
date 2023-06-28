8f4c6a618fa0eaa89c94a78e5e2125c3f6b94c1a
RSR <80620448+RSR-TG-Info@users.noreply.github.com>
2023-06-28 18:18:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,5 +2,5 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start"))
 async def bstart(client, message):
-    await client.send_message(chat_id=message.chat.id, text="start message :)", reply_to_message_id=message.id)
+    await client.send_message(chat_id=message.chat.id, text="Start message :)\n\nSource code: https://github.com/RSR-TG-Info/Telegram-Bot-Template", reply_to_message_id=message.id, disable_web_page_preview=True)
     return
